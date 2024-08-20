80b3656102a10a26e0a6e683718d5a956a96225f
Albert Vitor <albertvitor18@outlook.com>
2024-08-20 20:55:21 -0300

    Conteúdo do Projeto!

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,74 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from "vue";
+import Resultado from "./components/Resultado.vue";
+
+
+const values = reactive({
+  primeiroNumero: 0,
+  segundoNumero: 0,
+  operacao: 'adicao',
+  resultado: 0
+})
+
+function calcular() {
+  if (values.operacao === "adicao") {
+    values.resultado = values.primeiroNumero + values.segundoNumero
+  } else if (values.operacao === "subtracao") {
+    values.resultado = values.primeiroNumero - values.segundoNumero
+  } else if (values.operacao === "multiplicacao") {
+    values.resultado = values.primeiroNumero * values.segundoNumero
+  } else if (values.operacao === "divisao") {
+    values.resultado = values.primeiroNumero / values.segundoNumero
+  }
+}
+
+function escolha(op, e) {
+  if (op === "primeiro") {
+    if (e.target.value === '') {
+      values.primeiroNumero = 0
+    } else {
+      values.primeiroNumero = parseInt(e.target.value)
+    }
+    calcular()
+  } else if (op === "segundo") {
+    if (e.target.value === '') {
+      values.segundoNumero = 0
+    } else {
+      values.segundoNumero = parseInt(e.target.value)
+    }
+    calcular()
+  } else if (op === "operacao") {
+    values.operacao = e.target.value
+    calcular()
+  }
+}
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <div class="text-center mt-5">
+      <h1>Calculadora</h1>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="d-flex flex-column mt-4 align-items-center gap-3">
+      <input type="number" class="w-25 form-control" @input="(e) => escolha('primeiro', e)"
+        placeholder="Digite um número">
+      <input type="number" class="w-25 form-control" @input="(e) => escolha('segundo', e)"
+        placeholder="Digite um número">
+      <select class="w-25 form-control" @change="(e) => escolha('operacao', e)" id="">
+        <option value="adicao">Adição</option>
+        <option value="subtracao">Subtração</option>
+        <option value="multiplicacao">Multiplicação</option>
+        <option value="divisao">Divisão</option>
+      </select>
+    </div>
+
+    <div class="d-flex justify-content-center mt-4">
+      <resultado :resultado="values.resultado" />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
